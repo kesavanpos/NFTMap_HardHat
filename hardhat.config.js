d6d965a04420b54b4408fa19984a082e2c37a38f
krajendran <krajendran@explorance.com>
2022-11-17 21:28:13 +0530

    Alchemy api url

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,12 +5,20 @@ require("@nomicfoundation/hardhat-toolbox");
 // testing the frontend.
 require("./tasks/faucet");
 
+const API_URL='https://eth-goerli.g.alchemy.com/v2/foiRhE1OcCTdH4QMaUAI-tq9N4ImoErQ'
+const PRIVATE_KEY='9e4782b5efdc36e4edd96206b1c8698a5ea8d832892b3a11a34b13ec072e723c'
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
-    }
+    },
+    goerli: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+   }
   }
 };
